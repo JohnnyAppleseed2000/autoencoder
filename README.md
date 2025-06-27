cbdfc95d896b530a588ddb1bb96f7e0b5939cddd
johnh <johnhong2000107@naver.com>
2025-06-27 19:22:13 +0900

    3번째

--- README.md
+++ README.md
@@ -22,12 +22,12 @@ Kernel size = 3 , stride = 1 , padding = 1 을 사용.
 
 첫 Convolution과 max pooling을 통해 size (24,14,14)를 만듬
 두번째 convolution과 max pooling을 통해 size (48,7,7)
-세번째 convolution size(96,7,7)
+세번째 convolution size(96,3,3)
 flatten 한후 1차원의 512크기의 tensor로 축소.
 
 
 - Decoder 단게에선 역컨벌루션
-512크기의 1차원 텐서를 다시  (96,7,7)로 확장
+512크기의 1차원 텐서를 다시  (96,3,3)로 확장
 1차 역컨벌루션 (48,7,7) relu
 2차 역컨벌루션 (24,14,14) relu
 3차 역컨벌루션 (1,28,28) 으로 복원 
--- model.py
+++ model.py
@@ -1,33 +1,39 @@
-class Autoencoder(nn.module):
+import torch
+import torch.nn as nn
+
+class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
-        enc_size = 1024
+        self.enc_size = 512
         self.encoder = nn.Sequential(
-            nn.Conv2d(
-                in_channels=1,
-                out_channels=28,
-                kernel_size=3,
-                stride=1,
-                padding=1
-            ),
-            nn.Relu(),
-            nn.MaxPool2D(2,2),
-            # 출력크기 (28,14,14)
+            nn.Conv2d(in_channels=1, out_channels=24, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # (24, 14, 14)
 
-            nn.Conv2d(
-                in_channels=1,
-                out_channels=56,
-                kernel_size=3,
-                stride=1,
-                padding=1
-            ),
-            nn.Relu(),
-            nn.MaxPool2D(2, 2)
-            # 출력크기 (56,7,7)
+            nn.Conv2d(in_channels=24, out_channels=48, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # (48, 7, 7)
 
-        nn.Flatten(),
-        nn.Linear(56 * 7 * 7, enc_size)
+            nn.Conv2d(in_channels=48, out_channels=96, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # (96, 3, 3)
+
+            nn.Flatten(),
+            nn.Linear(96 * 3 * 3, self.enc_size)
         )
 
-        self.decoder = nn.Sequential
+        self.decoder = nn.Sequential(
+            nn.Linear(self.enc_size, 96 * 3 * 3),
+            nn.Unflatten(1, (96, 3, 3)),
+            nn.ConvTranspose2d(96, 48, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(48, 24, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.Conv2d(24, 1, kernel_size=3, padding=1),
+            nn.Sigmoid()
+        )
 
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
